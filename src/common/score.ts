862cd1097c10b101a194dce8f96df10ee50067a8
Alexi Christakis <alexi.christakis@gmail.com>
2024-04-11 15:51:11 -0400

    update words

--- src/common/score.ts
+++ src/common/score.ts
@@ -1,6 +1,6 @@
+// TODO
 export const scoreWord = (word: string): number => (word.length - 2) * 100;
 
-export const scoreWords = (words: Iterable<string>): number =>
-  Array.from(words).reduce((score, word) => {
-    return score + scoreWord(word);
-  }, 0);
+export const scoreWords = (words: Iterable<string>): number => {
+  return Array.from(words).reduce((score, word) => score + scoreWord(word), 0);
+};
